a5aa4faa51a7d1525945af3ad79b5d60bc83fd23
Junzhe Chen <141964509+Junzhe-Chen@users.noreply.github.com>
2023-08-12 07:51:31 +0100

    Minor code changes to make it look better

--- Class_D_modulator_MATLAB.m
+++ Class_D_modulator_MATLAB.m
@@ -1,14 +1,14 @@
 %% Initialise
-clear all;
 close all;
+clear;
 clc;
 
 %% Sine wave (as the I/P signal) generation
 
 % Parameters
 sineFrequency = 1; % Frequency in Hz Default set as standard 1kHz
-sineNumPoints = 5000; % Number of data points
-sineSamplingRate = 1000* sineFrequency; % Sampling rate in Hz, 1000 points per cycle
+sineNumPoints = 50000; % Number of data points
+sineSamplingRate = 10000* sineFrequency; % Sampling rate in Hz, 1000 points per cycle
 t = (0:sineNumPoints-1) / sineSamplingRate;% Define time axis
 
 sineAmplitude = 0.5; % Amplitude of Sine wave
@@ -22,11 +22,12 @@ sineWaveSignal = sineAmplitude * sin(2 * pi * sineFrequency * t); % Sine wave ou
 modulatingMultiplier = 100; 
 
 % % Plot the sine wave
-% plot(t, sineWaveSignal);
-% xlabel('Time (s)');
-% ylabel('Amplitude');
-% title('Discrete Sine Wave');
-% grid on;
+figure();
+plot(t, sineWaveSignal, "LineWidth", 2, "Color", "blue");
+xlabel("Time (s)");
+ylabel("Amplitude");
+title("Input sine wave");
+grid on;
 
 %% Sawtooth wave generation
 % Parameters
@@ -40,11 +41,12 @@ sawtoothAmplitude = 1; % Amplitude of the sawtooth wave
 sawtoothWaveReference = sawtoothAmplitude * sawtooth(2 * pi * sawtoothFrequency * t, 1);
 
 % % Plot the sawtooth wave
-% plot(t, sawtoothWaveReference);
-% xlabel('Time (s)');
-% ylabel('Amplitude');
-% title('Discrete Sawtooth Wave');
-% grid on;
+figure();
+plot(t, sawtoothWaveReference, "LineWidth", 2, "Color", "blue");
+xlabel("Time (s)");
+ylabel("Amplitude");
+title("Sawtooth wave reference");
+grid on;
 
 %% Testbench, plotting sine and sawtooth wave in one graph
 % plot(t, sineWaveSignal);
@@ -66,24 +68,48 @@ outputVal = zeros(1,sineNumPoints);
      end
  end
 
- %% Drawing out the figure
- figure();
-plot(t, sawtoothWaveReference)
+ %% Drawing out the figure that shows the modulation process
+figure();
+plot(t, sawtoothWaveReference, "LineWidth", 2, "Color", "blue"); % Sawtooth wave
 hold on;
-plot(t, sineWaveSignal)
+plot(t, sineWaveSignal, "LineWidth", 0.5, "Color", "magenta"); % Sine wave
 hold on;
-plot(t, outputVal, 'LineWidth',2)
+plot(t, outputVal, 'LineWidth', 2, 'Color', "black"); % PWM output
+xlabel("Time (s)");
+ylabel("Amplitude (V)");
+title("PWM modulator plot");
+legend("Signal IN", "Sawtooth REF", "PWM OUT");
 hold off;
-grid on
+grid on;
 
 %% PWM and filtering results
 figure();
 plot(t,outputVal,'LineWidth',1,'Color','cyan')
 
 lowpass_output = lowpass(outputVal, 0.01);
-second_lowpass_output = lowpass(lowpass_output, 0.01);
-mean_output = movmean(lowpass_output,500);
+second_lowpass_output = lowpass(lowpass_output, 0.01); % Filtering twice to get -40dB/decade
+mean_output = movmean(second_lowpass_output, 500);
 
 hold on;
-plot(t, mean_output,'LineWidth',3,'Color','blue')
+plot(t, mean_output,'LineWidth',3,'Color','blue');
 hold off;
+
+grid on;
+xlabel("Time (s)");
+ylabel("Amplitude (V)");
+legend("PWM OUT", "Filtered OUT");
+title("PWM modulation waveform before/after filter");
+
+%% Comparison of the original signal and the reconstructed signal
+% Also consider adding FFT function to the code
+diffSignal = sineWaveSignal - mean_output;
+plot(t, mean_output, "LineWidth", 5, "Color", "magenta");
+hold on;
+plot(t, sineWaveSignal, "LineWidth", 1.5, "Color", "black");
+hold on;
+plot(t, diffSignal);
+hold off;
+xlabel("Time (s)");
+ylabel("Amplitude (V)");
+legend("Signal IN", "PWM OUT", "I/P and O/P Difference");
+title("Comparison between input and output signal");
